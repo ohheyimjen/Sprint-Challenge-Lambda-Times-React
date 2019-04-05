0d15982deeb0aa08a1620880c9ead065f4e86923
ohheyimjen <jendority@gmail.com>
2019-04-05 16:31:51 -0700

    Changed all css to styled-components in Header.js

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,45 @@
 import React from 'react';
-
+import sc from 'styled-components';
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
+const HeaderDIV = sc.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+`;
+
+const HeaderSPAN = sc.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
+
+const HeaderH1 = sc.h1`
+  font-family: Didot, serif;
+  font-weight: bold;
+  flex: 8;
+  text-align: center;
+  color: #000;
+`;
+
+
+
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
+    <HeaderDIV>
+      <HeaderSPAN>SMARCH 32, 2018</HeaderSPAN>
+      <HeaderH1>Lambda Times</HeaderH1>
       <span className="temp">98°</span>
-    </div>
+    </HeaderDIV>
   )
 }
 
